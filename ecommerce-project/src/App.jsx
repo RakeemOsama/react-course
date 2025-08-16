4904fafdbe6431ae6a839106986682c48f3494a9
RakeemOsama <heavenlyecstacy06@gmail.com>
2025-08-16 16:34:20 +0200

    Moved orders page to react

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { ChekcoutPage } from './pages/CheckoutPage'
-import './App.css'
 import { TrackingPage } from './pages/TrackingPage'
+import { OrdersPage } from './pages/OrdersPage'
+import './App.css'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<ChekcoutPage />} />
       <Route path="tracking" element={<TrackingPage />} />
+      <Route path="orders" element={<OrdersPage />} />
     </Routes>
     
   )
--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,3 +1,4 @@
+import './header.css'
 import './TrackingPage.css'
 
 export function TrackingPage() {
